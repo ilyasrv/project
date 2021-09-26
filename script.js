93fe0035bd3c85a3221c4cda41573258dcfa8583
Ilya <srv9480@gmail.com>
2021-09-26 13:24:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+  
+let something; 
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("Hello World");
+
+let answer = prompt("are you here?", "da");
+
+console.log(typeof(null));
